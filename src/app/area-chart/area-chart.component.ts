9b6d26d7957af72fa235e572521849f4e3d19fb7
rinsha165 <rinshrinshak@gmail.com>
2023-04-26 11:19:05 +0530

    admin dashboard using angular material,highcharts

---
+++ src/app/area-chart/area-chart.component.ts
@@ -0,0 +1,68 @@
+import { Component } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import HC_exporting from 'highcharts/modules/exporting';
+
+@Component({
+  selector: 'app-area-chart',
+  templateUrl: './area-chart.component.html',
+  styleUrls: ['./area-chart.component.css']
+})
+export class AreaChartComponent {
+  Highcharts = Highcharts
+  chartOptions={}
+  constructor(){
+    this.chartOptions={
+      chart: {
+        renderTo: 'container',
+        type: 'column',
+        options3d: {
+            enabled: true,
+            alpha: 15,
+            beta: 15,
+            depth: 50,
+            viewDistance: 25
+        }
+    },
+    xAxis: {
+        categories: ['MEARN', 'PYTHON', 'DATA SCIENCE', 'TESTING', 'FLUTTER', '.NET',
+             'JAVA SPRING', 'BIG DATA ','ML', 'AI']
+    },
+    yAxis: {
+        title: {
+            enabled: false
+        }
+    },
+    tooltip: {
+        headerFormat: '<b>{point.key}</b><br>',
+        pointFormat: 'Placements: {point.y}'
+    },
+    title: {
+        text: 'yearly placement analysis , 2022-2023',
+        align: 'center'
+    },
+    subtitle: {
+        text: 'Luminar Technolab ' ,
+           
+        align: 'center'
+    },
+    legend: {
+        enabled: false
+    },
+    credits:{
+      enabled: false
+    },
+    plotOptions: {
+        column: {
+            depth: 25
+        }
+    },
+    series: [{
+        data: [1318, 1073, 1060, 813, 775, 745, 537, 444, 416, 395],
+        colorByPoint: true
+    }]
+    }
+    // to export chart
+    HC_exporting(Highcharts);
+  }
+
+}
---
+++ src/app/pie-chart/pie-chart.component.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import * as Highcharts from 'highcharts';
+
+
+@Component({
+  selector: 'app-pie-chart',
+  templateUrl: './pie-chart.component.html',
+  styleUrls: ['./pie-chart.component.css']
+})
+export class PieChartComponent {
+  Highcharts = Highcharts
+  chartOptions={}
+   
+  constructor(){
+    this.chartOptions = {
+      // give chart configure data from highchart
+      chart: {
+        type: 'pie',
+        options3d: {
+            enabled: true,
+            alpha: 45,
+            beta: 0
+        }
+    },
+    title: {
+        text: 'average students admission , 2022-2023',
+        align: 'left'
+    },
+    
+    accessibility: {
+        point: {
+            valueSuffix: '%'
+        }
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            depth: 35,
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}'
+            }
+        }
+    },
+    credits:{
+      enabled:false
+    },
+    series: [{
+        type: 'pie',
+        name: 'count',
+        data: [
+            ['MEARN', 23],
+            ['PYTHON', 18],
+            ['ASP.NET', 8],
+            ['TESTING', 20],
+            ['FLUTTER', 7],
+            ['BIG DATA', 30]
+        ]
+    }]
+    }
+  }
+}
